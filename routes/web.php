986b1004c197585ce9942173b11a6083d1e95b7f
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-10-12 13:10:05 +0900

    Herokuへのデプロイ準備

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 require __DIR__.'/auth.php';
