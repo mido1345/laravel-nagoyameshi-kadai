037359e80ef353a9e1677af65a11414987a52c7f
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-10-12 17:16:53 +0900

    管理者側の認証

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -40,9 +40,11 @@ class LoginRequest extends FormRequest
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
+        $this->is('admin/*') ? $guard = 'admin' : $guard = 'web';
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::guard($guard)->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
+
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
