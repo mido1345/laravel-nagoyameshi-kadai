75b3e6da7346312f5658a0a76b4ee228fef3b030
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-10-15 17:46:50 +0900

    変更

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(100)->create();
+    }
+}
