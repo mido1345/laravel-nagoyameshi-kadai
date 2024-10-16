ea9c6176afb1372532d7faca3a6fd748d71ce910
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-10-16 15:15:14 +0900

    管理者側の店舗管理機能を作ろう

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Restaurant;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    //
+}
