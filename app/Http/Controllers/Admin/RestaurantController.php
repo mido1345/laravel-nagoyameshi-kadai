376d12528accdcf26912e0e9efec18211052566d
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-10-16 17:37:17 +0900

    管理者側の店舗管理機能を作ろう

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -8,5 +8,125 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index(Request $request) {
+        
+        $keyword = $request->input('keyword');
+        
+        if ($keyword) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")->paginate(15);
+        } 
+        else {
+            $restaurants = Restaurant::paginate(15);
+        }
+
+        $total = $restaurants->total();
+        return view('admin.restaurants.index', compact('restaurants', 'keyword', 'total'));
+    }
+    
+    public function show(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.show', compact('restaurant'));
+    }
+
+    public function create(Request $request)
+    {
+        return view('admin.restaurants.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'image' => 'file|mimes:jpg、jpeg、png、bmp、gif、svg、webp|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0',
+        ]);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->input('name');
+
+        // アップロードされたファイル（name="image"）が存在すれば処理を実行する
+        if ($request->hasFile('image')) {
+            // アップロードされたファイル（name="image"）をstorage/app/public/restaurantsフォルダに保存し、戻り値（ファイルパス）を変数$image_pathに代入する
+            $image_path = $request->file('image')->store('public/restaurants');
+            // ファイルパスからファイル名のみを取得し、restaurantsインスタンスのimage_nameプロパティに代入する
+            $product->image_name = basename($image_path);
+        }
+        else {
+            $restaurant->image = '';
+        }
+
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+        $restaurant->save();
+
+        
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を登録しました。');
+    }
+
+    public function edit(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.edit', compact('restaurant'));
+    }
+
+    public function update(Request $request, Restaurant $restaurant)
+    {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'file|mimes:jpg,jpeg,png,bmp,gif,svg,webp|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0'
+        ]);
+        
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image = basename($image);
+        }
+        $restaurant->update();
+
+        $category_ids = array_filter($request->input('category_ids'));
+        $restaurant->categories()->sync($category_ids);
+
+        $regular_holiday_ids = array_filter($request->input('regular_holiday_ids'));
+        $restaurant->regular_holidays()->sync($regular_holiday_ids);
+
+        return redirect()->route('admin.restaurants.show', compact('restaurant'))->with('flash_message', '店舗を編集しました。');
+    }
+
+    public function destroy(Restaurant $restaurant)
+    {
+        $restaurant->delete();
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を削除しました。');
+    }
 }
---
+++ tests/Feature/Admin/Auth/RestaurantTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Admin;
+use App\Models\Restaurant;
+
+class RestaurantTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
