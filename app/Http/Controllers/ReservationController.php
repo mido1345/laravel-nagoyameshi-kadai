b3475a816e8c74ea1fe6d2989de1e026a9ea2941
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-11-03 08:06:47 +0900

    訂正

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,44 +11,35 @@ use Illuminate\Http\Request;
 class ReservationController extends Controller
 {
     //indexアクション（予約一覧ページ）
-    public function index() 
-    {
-        $reservations = Reservation::where('user_id', auth()->id())
-            ->orderBy('reserved_datetime', 'desc')
-            ->paginate(15);
+    public function index() {
+        $reservations = Auth::user()->reservations()->orderBy('reserved_datetime', 'desc')->paginate(15);
 
         return view('reservations.index', compact('reservations'));
     }
 
-    //createアクション（予約ページ）
-    public function create(Restaurant $restaurant) 
-    {
+    public function create(Restaurant $restaurant) {
         return view('reservations.create', compact('restaurant'));
     }
 
-    //storeアクション（予約機能）
-    public function store(Request $request, Restaurant $restaurant) 
-    {
+    public function store(Request $request, Restaurant $restaurant) {
         $request->validate([
             'reservation_date' => 'required|date_format:Y-m-d',
             'reservation_time' => 'required|date_format:H:i',
-            'number_of_people' => 'required|integer|min:1|max:50',
+            'number_of_people' => 'required|numeric|between:1,50',
         ]);
 
-        Reservation::create([
-            'reserved_datetime' => $request->input('reservation_date'). ' ' . $request->input('reservation_time'),
-            'number_of_people' => $request->input('number_of_people'),
-            'restaurant_id' => $restaurant->id,
-            'user_id' => auth()->id(),
-        ]);
+        $reservation = new Reservation();
+        $reservation->reserved_datetime = $request->input('reservation_date') . ' ' . $request->input('reservation_time');
+        $reservation->number_of_people = $request->input('number_of_people');
+        $reservation->restaurant_id = $restaurant->id;
+        $reservation->user_id = $request->user()->id;
+        $reservation->save();
 
         return redirect()->route('reservations.index')->with('flash_message', '予約が完了しました。');
     }
 
-    //destroyアクション（予約キャンセル機能）
-    public function destroy(Restaurant $restaurant, Reservation $reservation) 
-    {
-        if ($reservation->user_id !== auth()->id()) {
+    public function destroy(Reservation $reservation) {
+        if ($reservation->user_id !== Auth::id()) {
             return redirect()->route('reservations.index')->with('error_message', '不正なアクセスです。');
         }
 
